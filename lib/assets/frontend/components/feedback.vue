52dac62b799b3a3eb1fef9cd5d7de74d9dfbb0d5
tean <tean@tarantula.tech>
2017-12-25 16:48:04 +0800

    *hotfix) 优化 feedback表格逻辑

--- lib/assets/frontend/components/feedback.vue
+++ lib/assets/frontend/components/feedback.vue
@@ -4,37 +4,42 @@
     <v-layout row>
       <v-flex xs12 sm10 offset-sm1>
         <v-card class='elevation-1' flat>
-           <v-container grid-list-md>
-             <v-layout row wrap>
-               <v-flex xs12>
-                 <v-text-field
-                   v-model="email"
-                   box
-                   :label="$t('feedback.email')"
-                   :rules="emailRules"
-                   ref='email'
-                   persistent-hint
-                 ></v-text-field>
-               </v-flex>
-               <v-flex xs12>
-                 <v-text-field
-                   v-model="content"
-                   box
-                   multi-line
-                   ref='content'
-                   :label="$t('feedback.content')"
-                 ></v-text-field>
-               </v-flex>
-             </v-layout>
-           </v-container>
+          <v-form v-model="valid" ref="form" lazy-validation>
+            <v-container grid-list-md>
+              <v-layout row wrap>
+                <v-flex xs12>
+                  <v-text-field
+                    v-model="email"
+                    box
+                    :label="$t('feedback.email')"
+                    :rules="emailRules"
+                    ref='email'
+                    persistent-hint
+                    required
+                  ></v-text-field>
+                </v-flex>
+                <v-flex xs12>
+                  <v-text-field
+                    v-model="content"
+                    box
+                    multi-line
+                    :rules="contentRules"
+                    ref='content'
+                    :label="$t('feedback.content')"
+                    required
+                  ></v-text-field>
+                </v-flex>
+              </v-layout>
+            </v-container>
 
-           <v-divider class="mt-5"></v-divider>
+            <v-divider class="mt-5"></v-divider>
             <v-card-actions>
               <v-spacer></v-spacer>
-              <v-btn color="primary" flat @click="submit" :disabled="formHasErrors ? true : false">{{ $t("string.submit")}}</v-btn>
+              <v-btn color="primary" flat @click="submit" :disabled="valid ? false : true">{{ $t("string.submit")}}</v-btn>
             </v-card-actions>
+          </v-form>
          </v-card>
-      </v-flex>
+       </v-flex>
     </v-layout>
     </v-container>
   </v-content>
@@ -52,57 +57,26 @@ export default {
 
   data(){
     return {
+      valid: false,
       email: '',
       emailRules: [
-         v => {
-           return !!v || this.$t('feedback.emailHint')
-         },
-         v => /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(v) || this.$t('feedback.emailHint')
+         v => !!v || this.$t('feedback.emailHint'),
+         v => /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/.test(v) || this.$t('feedback.emailHint')
       ],
       content: "",
-
-      formHasErrors: true
-    }
-  },
-
-  watch: {
-    email(v1, v2){
-      this.verifyForm()
-    },
-
-    content(v1, v2){
-      this.verifyForm()
-    }
-  },
-
-  computed: {
-    form () {
-      return {
-        email: this.email,
-        content: this.content,
-      }
+      contentRules: [
+        v => (v || "").length >= 10 || ""
+      ],
     }
   },
 
   methods: {
-    verifyForm(){
-      this.formHasErrors = false
-
-      Object.keys(this.form).forEach(f => {
-        if (!this.form[f]){
-          this.formHasErrors = true
-        }
-
-        this.$refs[f].validate(true)
-      })
-    },
-
     submit(){
-      this.formHasErrors = true
+      this.valid = false
       postFeedback({email: this.email, content: this.content}).then((r) => {
-        this.formHasErrors = false
-      }).catch((e) => {
-        this.formHasErrors = false
+        this.$refs.form.reset()
+      }).catch(() => {
+        this.valid = ture
       })
     }
   },
